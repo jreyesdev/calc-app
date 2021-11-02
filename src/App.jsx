34ca5241a7cd7aece8108a2eee8de085b10d2ab2
jreyesdev <djrm1204@gmail.com>
2021-11-01 23:43:03 -0400

    CSS y layout completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import './App.css';
+
 const App = () => {
-  return <h1>Calc App</h1>;
+  return (
+      <main className="react-calculator">
+        <div className="result"></div>
+        <div className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>Clear</button>
+            <button>r</button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>+</button>
+            <button>=</button>
+        </div>
+      </main>
+  );
 };
 
 export default App;
