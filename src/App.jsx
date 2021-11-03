6c0f7383e74a4b39a97f10252d2f057815bbbaeb
jreyesdev <djrm1204@gmail.com>
2021-11-02 22:05:05 -0400

    calculadora completa. realiza operaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Functions from "./components/Functions";
 import MathOperations from "./components/MathOperations";
 import Numbers from "./components/Numbers";
@@ -6,18 +8,65 @@ import Result from "./components/Result";
 import "./App.css";
 
 const App = () => {
+  const [stack, setStack] = useState("");
+  // Asigna resultado
   const clickHandler = (e) => {
-    console.log(e);
+    if (stack === "" && e === "0") return false;
+    setStack(stack + e);
   };
+  // Realiza operacion
   const handlerEqual = (e) => {
-    console.log(e);
+    const re = new RegExp(/[\+\-\*\/]/, "g");
+    const op = stack.search(re),
+      nums = stack.split(stack[op]),
+      [num1, num2] = nums;
+    let res = null;
+    console.log(op);
+    console.log(num1);
+    console.log(num2);
+    switch (stack[op]) {
+      case "+":
+        res = Number(num1) + Number(num2);
+        break;
+      case "-":
+        res = Number(num1) - Number(num2);
+        break;
+      case "*":
+        res = Number(num1) * Number(num2);
+        break;
+      case "/":
+        res = num2 === "0" ? "Error" : Number(num1) / Number(num2);
+        break;
+      default:
+        res = "Error";
+        break;
+    }
+    setStack(res.toString());
   };
-  const handlerOperation = (e) => {};
-  const handlerClear = (e) => {};
-  const handlerDelete = (e) => {};
+  // Asigna operacion
+  const handlerOperation = (op) => {
+    const index = stack.search(/[\+\-\*\/]/g),
+      ops = ["+", "-", "*", "/"];
+    let newOp = "";
+    if (index > 0) {
+      // Ya tiene operacion
+      // Sale si el ultimo caracter no es operacion
+      if (!ops.includes(stack[stack.length - 1])) return false;
+      // Cambia operacion
+      newOp = stack.slice(0, stack.length - 1) + op;
+    } else {
+      // No tiene operacion, la agrega
+      newOp = stack + op;
+    }
+    setStack(newOp);
+  };
+  // Limpia pantalla
+  const handlerClear = (e) => setStack("");
+  // Borra numero u operacion
+  const handlerDelete = (e) => setStack(stack.slice(0, stack.length - 1));
   return (
     <main className="react-calculator">
-      <Result value={""} />
+      <Result value={stack} />
       <Numbers clickNumber={clickHandler} />
       <Functions onClear={handlerClear} onDelete={handlerDelete} />
       <MathOperations
